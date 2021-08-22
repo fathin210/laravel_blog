b1e5fb4be035389f2c55e5adf7c5a7e2fa8a3e09
fathin210 <fathin210@gmail.com>
2021-08-22 11:04:56 +0700

    if else pada blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout title="Home Page">
+    @slot("styles")
+        <style>
+            body {
+                background-color: indigo;
+            }
+        </style>
+    @endslot
     <h1>Home</h1>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} | FATDEV</title>
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <link rel="stylesheet" href="/css/app.css">
+        {{ $styles }}
 </head>
 <body>
     <x-navbar/>
 
     {{ $slot }}
 
-    <script src="{{ asset("js/app.js") }}"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>

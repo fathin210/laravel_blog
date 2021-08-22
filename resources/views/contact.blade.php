f47a18f46bdc7cdbce8f5f4a4cc79d81a4bf176d
fathin210 <fathin210@gmail.com>
2021-08-22 15:18:54 +0700

    first controller

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout title="Contact">
     <h1>Contact</h1>
+
+    <form action="/contact" method="post">
+    @csrf
+
+    <button type="submit">Submit</button>
+    </form>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view("home"));
+Route::get('/', HomeController::class);
 
-Route::view('contact', 'contact');
-Route::view('about', 'about');
-
-Route::get('profile/{username}', function ($username) {
-    return view("profile", ["name" => $username]);
-});
-
-Route::get('profile', function (Request $request) {
-    $name = $request->name;
-    return view("profile",compact("name"));
-});
+Route::get('contact', [ContactController::class, 'create']);
+Route::post('contact', [ContactController::class, 'store']);

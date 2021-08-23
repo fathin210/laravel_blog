6e2ab32ff8e24509b7615d30c1248a23727bb3cf
fathin210 <fathin210@gmail.com>
2021-08-23 10:54:46 +0700

    Laravel Mix dan Bootstrap dengan webpack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} | FATDEV</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
         {{ $styles }}
 </head>
 <body>
@@ -13,6 +13,6 @@
     <div class="pt-4">
         {{ $slot }}
     </div>
-    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>

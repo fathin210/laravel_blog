46959cd5292a260779a7532a00d2c940e340ded5
fathin210 <fathin210@gmail.com>
2021-08-23 09:31:23 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(TasksSeeder::class);
+        $this->call(UsersSeeder::class);
     }
 }
